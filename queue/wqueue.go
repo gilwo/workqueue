f17290c5fdca35fae9f7bcd3c351204a7e9f9a74
Gil <gilwo@null.net>
2017-10-12 11:53:17 +0300

    formatting using go fmt

--- queue/wqueue.go
+++ queue/wqueue.go
@@ -1,111 +1,112 @@
 package workerqueue
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
 
 type QNode struct {
-    data interface{}
+	data interface{}
 }
 
 type Queue struct {
-    q []*QNode
-    count uint
-    capacity uint
-    lock *sync.Mutex
+	q        []*QNode
+	count    uint
+	capacity uint
+	lock     *sync.Mutex
 }
 
 func NewQueue(capacity uint) (q *Queue, err error) {
-    //TODO: add some chekcs ?
-    if capacity == 0 {
-        q = nil
-        err = fmt.Errorf("capacity is zero")
-        return
-    }
+	//TODO: add some chekcs ?
+	if capacity == 0 {
+		q = nil
+		err = fmt.Errorf("capacity is zero")
+		return
+	}
 
-    err = nil
-    q = &Queue{}
+	err = nil
+	q = &Queue{}
 
-    q.capacity = capacity
-    q.q = make([]*QNode, 0)
-    q.lock = &sync.Mutex{}
-    return
+	q.capacity = capacity
+	q.q = make([]*QNode, 0)
+	q.lock = &sync.Mutex{}
+	return
 }
 
 func (q *Queue) Count() uint {
-    return q.count
+	return q.count
 }
 
 func (q *Queue) Enqueue(data interface{}) {
-    q.lock.Lock()
-    defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-    e := &QNode{data}
-    q.q = append(q.q, e)
-    q.count += 1
+	e := &QNode{data}
+	q.q = append(q.q, e)
+	q.count += 1
 }
 
 func (q *Queue) Dequeue() (data interface{}, err error) {
-    q.lock.Lock()
-    defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-    if q.count == 0 {
-        err = fmt.Errorf("queue is empty")
-        data = nil
-        return
-    }
+	if q.count == 0 {
+		err = fmt.Errorf("queue is empty")
+		data = nil
+		return
+	}
 
-    err = nil
-    data = q.q[0].data
-    q.q = q.q[1:]
-    q.count -= 1
+	err = nil
+	data = q.q[0].data
+	q.q = q.q[1:]
+	q.count -= 1
 
-    return
+	return
 }
 
 func (q *Queue) Peek() (data interface{}, err error) {
-    q.lock.Lock()
-    defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-    if q.count == 0 {
-        err = fmt.Errorf("queue is empty")
-        data = nil
-        return
-    }
+	if q.count == 0 {
+		err = fmt.Errorf("queue is empty")
+		data = nil
+		return
+	}
 
-    err = nil
-    data = q.q[0].data
+	err = nil
+	data = q.q[0].data
 
-    return
+	return
 }
 
 func (q *Queue) Clear() {
-    q.lock.Lock()
-    defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-    q.count = 0
-    q.q = nil
-    q.q = make([]*QNode, 0)
+	q.count = 0
+	q.q = nil
+	q.q = make([]*QNode, 0)
 }
 
 func (q *Queue) Dispose() {
-    q.lock.Lock()
-    defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-    q.q = nil
-    q = nil
+	q.q = nil
+	q = nil
 }
 
 func (q *Queue) Dump() (ret string) {
-    q.lock.Lock()
-    defer q.lock.Unlock()
+	q.lock.Lock()
+	defer q.lock.Unlock()
 
-    if q.count == 0 {
-        ret = "queue is empty"
-    } else {
-    for i, e := range q.q {
-        ret += fmt.Sprintf("\tqueue[%d]: {%T - %v}\n", i, e.data, e.data)
-    }}
-    return
+	if q.count == 0 {
+		ret = "queue is empty"
+	} else {
+		for i, e := range q.q {
+			ret += fmt.Sprintf("\tqueue[%d]: {%T - %v}\n", i, e.data, e.data)
+		}
+	}
+	return
 }
--- queue/wqueue_test.go
+++ queue/wqueue_test.go
@@ -1,177 +1,176 @@
 package workerqueue
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestNewQueue(t *testing.T) {
-    if _, err := NewQueue(0); err != nil {
-        act := err.Error()
-        exp := "capacity is zero"
-        if act != exp {
-            t.Errorf("failed queue creation reason: expected : '%s', actual: '%s'",
-                    act, act)
-        }
-    }
+	if _, err := NewQueue(0); err != nil {
+		act := err.Error()
+		exp := "capacity is zero"
+		if act != exp {
+			t.Errorf("failed queue creation reason: expected : '%s', actual: '%s'",
+				act, act)
+		}
+	}
 
-    q, _ := NewQueue(2)
-    if q == nil {
-        t.Errorf("failed to create new queue")
-    }
-    if c := q.Count(); c != 0 {
-        t.Errorf("queue count mismatch, actual: %d, expected: %d", c, 0)
-    }
+	q, _ := NewQueue(2)
+	if q == nil {
+		t.Errorf("failed to create new queue")
+	}
+	if c := q.Count(); c != 0 {
+		t.Errorf("queue count mismatch, actual: %d, expected: %d", c, 0)
+	}
 
-    q.Dispose()
-    q = nil
+	q.Dispose()
+	q = nil
 }
 
 func TestPushAndPop(t *testing.T) {
-    q, _ := NewQueue(2)
+	q, _ := NewQueue(2)
 
-    var data int32 = 1
-    q.Enqueue("a")
-    q.Enqueue(data)
+	var data int32 = 1
+	q.Enqueue("a")
+	q.Enqueue(data)
 
-    v,_ := q.Dequeue()
-    if i, ok := v.(int32); ok {
-        t.Errorf("invalid type assertion: %T: %v", i, i)
-    } else {
-        //t.Logf("%v", ok)
-    }
+	v, _ := q.Dequeue()
+	if i, ok := v.(int32); ok {
+		t.Errorf("invalid type assertion: %T: %v", i, i)
+	} else {
+		//t.Logf("%v", ok)
+	}
 
-    v1,_ := q.Dequeue()
-    if v1.(int32) != data {
-        t.Errorf("value pushed and poped mismatch, pushed: %d, poped %d", data, v1)
-    }
+	v1, _ := q.Dequeue()
+	if v1.(int32) != data {
+		t.Errorf("value pushed and poped mismatch, pushed: %d, poped %d", data, v1)
+	}
 
-    if _, err := q.Dequeue(); err == nil {
-        t.Errorf("Empty Queue invalid Dequeue")
-    } else {
-        act := err.Error()
-        exp := "queue is empty"
-        if act != exp {
-            t.Errorf("Queue invalid Dequeue error mismatch, actual: '%s', expected: '%s'",
-                    act, exp)
-        }
-    }
+	if _, err := q.Dequeue(); err == nil {
+		t.Errorf("Empty Queue invalid Dequeue")
+	} else {
+		act := err.Error()
+		exp := "queue is empty"
+		if act != exp {
+			t.Errorf("Queue invalid Dequeue error mismatch, actual: '%s', expected: '%s'",
+				act, exp)
+		}
+	}
 
-    q.Dispose()
-    q = nil
+	q.Dispose()
+	q = nil
 }
 
 func TestCount(t *testing.T) {
-    q, _ := NewQueue(2)
+	q, _ := NewQueue(2)
 
-    if c:= q.Count(); c != 0 {
-        t.Errorf("queue count, actual: %d, expected: 0", c)
-    }
+	if c := q.Count(); c != 0 {
+		t.Errorf("queue count, actual: %d, expected: 0", c)
+	}
 
-    q.Enqueue(1)
-    q.Enqueue(2)
-    q.Enqueue(3)
+	q.Enqueue(1)
+	q.Enqueue(2)
+	q.Enqueue(3)
 
-    if c:= q.Count(); c != 3 {
-        t.Errorf("queue count, actual: %d, expected: 3", c)
-    }
+	if c := q.Count(); c != 3 {
+		t.Errorf("queue count, actual: %d, expected: 3", c)
+	}
 
-    q.Dequeue()
-    if c:= q.Count(); c != 2 {
-        t.Errorf("queue count, actual: %d, expected: 2", c)
-    }
+	q.Dequeue()
+	if c := q.Count(); c != 2 {
+		t.Errorf("queue count, actual: %d, expected: 2", c)
+	}
 
-    q.Dispose()
-    q = nil
+	q.Dispose()
+	q = nil
 }
 
 func TestPeek(t *testing.T) {
-    q, _ := NewQueue(2)
+	q, _ := NewQueue(2)
 
-    if c:= q.Count(); c != 0 {
-        t.Errorf("queue count, actual: %d, expected: 0", c)
-    }
+	if c := q.Count(); c != 0 {
+		t.Errorf("queue count, actual: %d, expected: 0", c)
+	}
 
-    d, err := q.Peek()
-    if err == nil {
-        t.Errorf("empty queue peek failed, expected: nil, actual: %v", d)
-    } else {
-        act := err.Error()
-        exp := "queue is empty"
-        if act != exp {
-            t.Errorf("Queue invalid peek error mismatch, actual: '%s', expected: '%s'",
-                    act, exp)
-        }
-    }
+	d, err := q.Peek()
+	if err == nil {
+		t.Errorf("empty queue peek failed, expected: nil, actual: %v", d)
+	} else {
+		act := err.Error()
+		exp := "queue is empty"
+		if act != exp {
+			t.Errorf("Queue invalid peek error mismatch, actual: '%s', expected: '%s'",
+				act, exp)
+		}
+	}
 
-    var data int32 = 1
-    q.Enqueue(data)
+	var data int32 = 1
+	q.Enqueue(data)
 
-    d, err = q.Peek()
-    if err != nil {
-        t.Errorf("queue peek failed, error %s", err)
-    } else {
-        if d.(int32) != data {
-            t.Errorf("Queue invalid peek, actual: %v, expected: %v",
-                    d, 1)
-        }
-    }
+	d, err = q.Peek()
+	if err != nil {
+		t.Errorf("queue peek failed, error %s", err)
+	} else {
+		if d.(int32) != data {
+			t.Errorf("Queue invalid peek, actual: %v, expected: %v",
+				d, 1)
+		}
+	}
 
-    q.Dispose()
-    q = nil
+	q.Dispose()
+	q = nil
 }
 
 func TestQueueClear(t *testing.T) {
-    q, _ := NewQueue(2)
+	q, _ := NewQueue(2)
 
-    if c:= q.Count(); c != 0 {
-        t.Errorf("queue count, actual: %d, expected: 0", c)
-    }
+	if c := q.Count(); c != 0 {
+		t.Errorf("queue count, actual: %d, expected: 0", c)
+	}
 
-    q.Enqueue(1)
+	q.Enqueue(1)
 
-    q.Clear()
+	q.Clear()
 
-    d, err := q.Peek()
-    if err == nil {
-        t.Errorf("cleared queue peek failed, expected: nil, actual: %v", d)
-    } else {
-        act := err.Error()
-        exp := "queue is empty"
-        if act != exp {
-            t.Errorf("Queue invalid peek error mismatch, actual: '%s', expected: '%s'",
-                    act, exp)
-        }
-    }
-    q.Dispose()
-    q = nil
+	d, err := q.Peek()
+	if err == nil {
+		t.Errorf("cleared queue peek failed, expected: nil, actual: %v", d)
+	} else {
+		act := err.Error()
+		exp := "queue is empty"
+		if act != exp {
+			t.Errorf("Queue invalid peek error mismatch, actual: '%s', expected: '%s'",
+				act, exp)
+		}
+	}
+	q.Dispose()
+	q = nil
 }
 
-
 func TestQueueDump(t *testing.T) {
-    q, _ := NewQueue(2)
+	q, _ := NewQueue(2)
 
-    if c:= q.Count(); c != 0 {
-        t.Errorf("queue count, actual: %d, expected: 0", c)
-    }
+	if c := q.Count(); c != 0 {
+		t.Errorf("queue count, actual: %d, expected: 0", c)
+	}
 
-    expdump := "queue is empty"
-    actdump := q.Dump()
-    if actdump != expdump {
-        t.Errorf("Queue dump mismatch, actual: '%s', expected: '%s'",
-                actdump, expdump)
-    }
+	expdump := "queue is empty"
+	actdump := q.Dump()
+	if actdump != expdump {
+		t.Errorf("Queue dump mismatch, actual: '%s', expected: '%s'",
+			actdump, expdump)
+	}
 
-    q.Enqueue(1)
+	q.Enqueue(1)
 
-    //fmt.Println(q.Dump())
+	//fmt.Println(q.Dump())
 
-    expdump = "\tqueue[0]: {int - 1}\n"
-    actdump = q.Dump()
-    if actdump != expdump {
-        t.Errorf("Queue dump mismatch, actual: '%s', expected: '%s'",
-                actdump, expdump)
-    }
+	expdump = "\tqueue[0]: {int - 1}\n"
+	actdump = q.Dump()
+	if actdump != expdump {
+		t.Errorf("Queue dump mismatch, actual: '%s', expected: '%s'",
+			actdump, expdump)
+	}
 
-    q.Dispose()
-    q = nil
+	q.Dispose()
+	q = nil
 }
