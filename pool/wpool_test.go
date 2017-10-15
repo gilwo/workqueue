e8df548a9992f71f6155698c075181067d6370b9
Gil <gilwo@null.net>
2017-10-16 00:24:25 +0300

    adding test for multiple jobs queueing and finishing

--- pool/wpool_test.go
+++ pool/wpool_test.go
@@ -36,7 +36,6 @@ func TestNewPool(t *testing.T) {
 
 	f := func(i interface{}) interface{} {
 		fmt.Printf("hello world\n")
-		//time.Sleep(2*time.Second)
 		hello = true
 		timerStopDispatcher.Stop()
 		go foo()
@@ -48,7 +47,6 @@ func TestNewPool(t *testing.T) {
 	if err != nil {
 		t.Errorf("job queue failed, err: %v", err)
 	} else {
-		//fmt.Printf("%T:%v\n", job, job)
 	}
 
 	go func() {
@@ -136,6 +134,92 @@ func TestJobFailToFinishInTime(t *testing.T) {
 		//t.Errorf("job function have not been called in reasonable time")
 		t.Errorf("job function was not suppose to be called")
 	}
+	_, err = wp.Dispose()
+	if err != nil {
+		t.Errorf("pool failed on dispose, err: %v\n", err)
+	}
+}
+
+func TestMultipleJobsQueue(t *testing.T) {
+	wp, err := NewWPool(10)
+	if err != nil {
+		t.Errorf("failed to create pool")
+	}
+
+	var hello [10]bool
+	var hellook bool
+
+	chello := sync.NewCond(&sync.Mutex{})
+
+	for i := 0; i < 10; i++ {
+		msg := fmt.Sprintf("hello world %d\n", i+1)
+		y := i
+		fmt.Printf("### queueing job %d\n", i+1)
+		wp.JobQueue(func(j interface{}) interface{} {
+			chello.L.Lock()
+			chello.Wait()
+			chello.L.Unlock()
+			fmt.Println(msg)
+			hello[y] = true
+			return nil
+		})
+	}
+
+	_, err = wp.StartDispatcher()
+	if err != nil {
+		t.Errorf("dispatcher failed to start: %v", err)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	chello.Broadcast()
+
+	cfinish := sync.NewCond(&sync.Mutex{})
+
+	var stopchecking bool
+
+	// finish it even if not finished
+	timerStop := time.AfterFunc(5*time.Second, func() {
+		stopchecking = true
+		cfinish.Signal()
+
+	})
+
+	go func() {
+		for !stopchecking {
+			select {
+			case <-time.After(1 * time.Second):
+			}
+			var ok bool = true
+			// check we did ok
+			for _, i := range hello {
+				ok = ok && i
+			}
+			hellook = ok
+			if ok {
+				timerStop.Stop()
+				cfinish.Signal()
+				break
+			}
+		}
+	}()
+
+	cfinish.L.Lock()
+	cfinish.Wait()
+	cfinish.L.Unlock()
+
+	wp.StopDispatcher(func() {
+		fmt.Println("dispatcher stopped")
+		cfinish.Signal()
+	})
+	if !hellook {
+		t.Errorf("not all hellos finished: %v", hello)
+	}
+
+	cfinish.L.Lock()
+	cfinish.Wait()
+	cfinish.L.Unlock()
+
 	_, err = wp.Dispose()
 	if err != nil {
 		t.Errorf("pool failed on dispose, err: %v\n", err)
