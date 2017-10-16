d66786e9c906a504298cce45dc18dc18f130633e
Gil <gilwo@null.net>
2017-10-16 21:29:16 +0300

    refactor tests

--- pool/wpool_test.go
+++ pool/wpool_test.go
@@ -7,24 +7,56 @@ import (
 	"time"
 )
 
-func TestNewPool(t *testing.T) {
-
-	wp, err := NewWPool(10)
+func startCheckPool(t *testing.T, count uint) *WPool {
+	wp, err := NewWPool(count)
 	if err != nil {
 		t.Errorf("failed to create pool")
 	}
+	return wp
+}
 
-	_, err = wp.StartDispatcher()
+func startCheckDispatcher(t *testing.T, wp *WPool) {
+	_, err := wp.StartDispatcher()
 	if err != nil {
 		t.Errorf("dispatcher failed to start: %v", err)
 	}
+}
+
+func checkJobQueue(t *testing.T, wp *WPool, f JobFunc) {
+	_, err := wp.JobQueue(f)
+
+	if err != nil {
+		t.Errorf("job queue failed, err: %v", err)
+	} else {
+	}
+}
+
+func checkPoolDispose(t *testing.T, wp *WPool) {
+	_, err := wp.Dispose()
+	if err != nil {
+		t.Errorf("pool failed on dispose, err: %v\n", err)
+	}
+}
+
+func stopCheckDispatcher(t *testing.T, wp *WPool, f func()) {
+	_, err := wp.StopDispatcher(f)
+	if err != nil {
+		t.Errorf("pool failed to stop, err: %v\n", err)
+	}
+}
+
+func TestNewPool(t *testing.T) {
+
+	wp := startCheckPool(t, 10)
+
+	startCheckDispatcher(t, wp)
 
 	condStopDispatcher := sync.NewCond(&sync.Mutex{})
 
 	var stopped bool
 
 	foo := func() {
-		wp.StopDispatcher(func() {
+		stopCheckDispatcher(t, wp, func() {
 			fmt.Println("pool dispatcher stopped")
 			stopped = true
 			condStopDispatcher.Signal()
@@ -42,12 +74,7 @@ func TestNewPool(t *testing.T) {
 		return nil
 	}
 
-	_, err = wp.JobQueue(f)
-
-	if err != nil {
-		t.Errorf("job queue failed, err: %v", err)
-	} else {
-	}
+	checkJobQueue(t, wp, f)
 
 	go func() {
 		select {
@@ -67,23 +94,14 @@ func TestNewPool(t *testing.T) {
 	if !hello {
 		t.Errorf("job function have not been called in reasonable time")
 	}
-	_, err = wp.Dispose()
-	if err != nil {
-		t.Errorf("pool failed on dispose, err: %v\n", err)
-	}
+	checkPoolDispose(t, wp)
 }
 
 func TestJobFailToFinishInTime(t *testing.T) {
 
-	wp, err := NewWPool(1)
-	if err != nil {
-		t.Errorf("failed to create pool")
-	}
+	wp := startCheckPool(t, 1)
 
-	_, err = wp.StartDispatcher()
-	if err != nil {
-		t.Errorf("dispatcher failed to start: %v", err)
-	}
+	startCheckDispatcher(t, wp)
 
 	var hello bool
 
@@ -94,11 +112,7 @@ func TestJobFailToFinishInTime(t *testing.T) {
 		return true
 	}
 
-	_, err = wp.JobQueue(f)
-
-	if err != nil {
-		t.Errorf("job queue failed, err: %v", err)
-	}
+	checkJobQueue(t, wp, f)
 
 	c := sync.NewCond(&sync.Mutex{})
 
@@ -107,7 +121,7 @@ func TestJobFailToFinishInTime(t *testing.T) {
 	go func() {
 		select {
 		case <-time.After(5 * time.Millisecond):
-			wp.StopDispatcher(func() {
+			stopCheckDispatcher(t, wp, func() {
 				fmt.Println("pool dispatcher stopped")
 				stopped = true
 				c.Signal()
@@ -131,20 +145,13 @@ func TestJobFailToFinishInTime(t *testing.T) {
 		t.Errorf("dispatcher did not stopped in reasonable time")
 	}
 	if hello {
-		//t.Errorf("job function have not been called in reasonable time")
 		t.Errorf("job function was not suppose to be called")
 	}
-	_, err = wp.Dispose()
-	if err != nil {
-		t.Errorf("pool failed on dispose, err: %v\n", err)
-	}
+	checkPoolDispose(t, wp)
 }
 
 func TestMultipleJobsQueue(t *testing.T) {
-	wp, err := NewWPool(10)
-	if err != nil {
-		t.Errorf("failed to create pool")
-	}
+	wp := startCheckPool(t, 10)
 
 	var hello [10]bool
 	var hellook bool
@@ -152,23 +159,22 @@ func TestMultipleJobsQueue(t *testing.T) {
 	chello := sync.NewCond(&sync.Mutex{})
 
 	for i := 0; i < 10; i++ {
-		msg := fmt.Sprintf("hello world %d\n", i+1)
+		msg := fmt.Sprintf("hello world %d", i+1)
 		y := i
 		fmt.Printf("### queueing job %d\n", i+1)
-		wp.JobQueue(func(j interface{}) interface{} {
+		checkJobQueue(t, wp, func(j interface{}) interface{} {
 			chello.L.Lock()
 			chello.Wait()
 			chello.L.Unlock()
 			fmt.Println(msg)
+			//if y != 0 {
 			hello[y] = true
+			//}
 			return nil
 		})
 	}
 
-	_, err = wp.StartDispatcher()
-	if err != nil {
-		t.Errorf("dispatcher failed to start: %v", err)
-	}
+	startCheckDispatcher(t, wp)
 
 	time.Sleep(1 * time.Second)
 
@@ -208,7 +214,7 @@ func TestMultipleJobsQueue(t *testing.T) {
 	cfinish.Wait()
 	cfinish.L.Unlock()
 
-	wp.StopDispatcher(func() {
+	stopCheckDispatcher(t, wp, func() {
 		fmt.Println("dispatcher stopped")
 		cfinish.Signal()
 	})
@@ -220,8 +226,5 @@ func TestMultipleJobsQueue(t *testing.T) {
 	cfinish.Wait()
 	cfinish.L.Unlock()
 
-	_, err = wp.Dispose()
-	if err != nil {
-		t.Errorf("pool failed on dispose, err: %v\n", err)
-	}
+	checkPoolDispose(t, wp)
 }
